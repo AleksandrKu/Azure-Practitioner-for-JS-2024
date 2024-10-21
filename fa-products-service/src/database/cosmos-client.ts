b331e2ce40f1d45039f32ec07471045a9c4d933a
Oleksandr_Kunup <Oleksandr_Kunup@epam.com>
2024-10-21 17:23:39 +0300

    Add managed identity

--- fa-products-service/src/database/cosmos-client.ts
+++ fa-products-service/src/database/cosmos-client.ts
@@ -1,10 +1,16 @@
 import { CosmosClient } from '@azure/cosmos';
+import { DefaultAzureCredential } from '@azure/identity';
+const credential = new DefaultAzureCredential();
 
 const endpoint = process.env.COSMOS_ENDPOINT || "";
 const key = process.env.COSMOS_KEY || "";
-const databaseId = 'products-db';
-
-export const client = new CosmosClient({ endpoint, key });
+const databaseId = "products-db";
+let client: CosmosClient;
+if (process.env.IS_LOCAL === "true") {
+    client = new CosmosClient({ endpoint, key });
+} else {
+    client = new CosmosClient({ endpoint, aadCredentials: credential });
+}
 export const database = client.database(databaseId);
 export const productsContainer = database.container('products');
 export const stocksContainer = database.container('stocks');
