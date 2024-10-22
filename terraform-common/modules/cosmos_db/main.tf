5421397faf8a38c8e2a35a0c0335ed9670ee58f6
Oleksandr_Kunup <Oleksandr_Kunup@epam.com>
2024-10-23 00:31:18 +0300

    Add cosmosDB Managed Identity of functions app

--- terraform-common/modules/cosmos_db/main.tf
+++ terraform-common/modules/cosmos_db/main.tf
@@ -19,6 +19,44 @@ resource "azurerm_cosmosdb_account" "cosmosdb_account" {
   }
 }
 
+resource "azurerm_cosmosdb_sql_role_definition" "products_app_role" {
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account.name
+  name                = "ProductsReadMetadataRole"
+  type                = "CustomRole"
+  assignable_scopes   = [azurerm_cosmosdb_account.cosmosdb_account.id]
+
+  permissions {
+    data_actions = [
+      "Microsoft.DocumentDB/databaseAccounts/readMetadata",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/*",
+      "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/*"
+    ]
+  }
+}
+
+# Role assignment for the Managed Identity of your Function App
+resource "azurerm_cosmosdb_sql_role_assignment" "fa_products_staging_read_metadata" {
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account.name
+  role_definition_id  = azurerm_cosmosdb_sql_role_definition.products_app_role.id
+  principal_id        = "d5421087-31fd-4a21-b260-04ab933b6e56"
+  scope               = azurerm_cosmosdb_account.cosmosdb_account.id
+
+  depends_on = [azurerm_cosmosdb_sql_role_definition.products_app_role]
+}
+
+# Role assignment for the Managed Identity of your Function App
+resource "azurerm_cosmosdb_sql_role_assignment" "fa_products_read_metadata" {
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account.name
+  role_definition_id  = azurerm_cosmosdb_sql_role_definition.products_app_role.id
+  principal_id        = "3798d888-e842-4b05-9995-9c33dbc57a0b"
+  scope               = azurerm_cosmosdb_account.cosmosdb_account.id
+
+  depends_on = [azurerm_cosmosdb_sql_role_definition.products_app_role]
+}
+
 resource "azurerm_cosmosdb_sql_database" "products_app" {
   name                = "products-db"
   resource_group_name = var.resource_group_name
